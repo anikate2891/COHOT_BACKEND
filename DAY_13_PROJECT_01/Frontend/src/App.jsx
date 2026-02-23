e4316ded89e4380a2325375116c1d83a85f24f27
Anikate Kanoo <anikate955@gmail.com>
2026-02-23 10:38:14 +0530

    data added

---
+++ DAY_13_PROJECT_01/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router"
+import AppRouths from './AppRoutes'
+import './style.scss'
+
+const App = () => {
+  return (
+    <>
+      <AppRouths/>
+    </>
+  )
+}
+
+export default App
