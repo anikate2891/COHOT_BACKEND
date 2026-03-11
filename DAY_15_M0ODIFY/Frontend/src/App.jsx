4d004ee3ef77faa5cf99846335b2eadeb3cc96a5
Anikate Kanoo <anikate955@gmail.com>
2026-03-11 20:15:01 +0530

    added

---
+++ DAY_15_M0ODIFY/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FaceExpression from './features/Expression/components/Face.expression'
+
+function App() {
+
+  return (
+   <>
+      <FaceExpression />
+   </>
+  )
+}
+
+export default App
