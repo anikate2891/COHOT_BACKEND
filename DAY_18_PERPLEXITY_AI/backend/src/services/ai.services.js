3b753663ad5d3069ccdc21287bf4be8b0eaa12ee
Anikate Kanoo <anikate955@gmail.com>
2026-03-17 13:02:15 +0530

    feat: integrate socket.io for real-time chat functionality
    - Added socket.io server initialization in backend with CORS configuration.
    - Implemented chat message handling in chat.controller.js.
    - Created chat routes for sending messages and fetching chats.
    - Established socket connection in frontend using socket.io-client.
    - Updated DashBoard component to initialize socket connection on mount.
    - Added chat service for managing socket interactions.
    - Included new dependencies for socket.io in both frontend and backend package.json files.

--- DAY_18_PERPLEXITY_AI/backend/src/services/ai.services.js
+++ DAY_18_PERPLEXITY_AI/backend/src/services/ai.services.js
@@ -1,11 +1,33 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage, SystemMessage, AIMessage } from "@langchain/core/messages"
+import { ChatMistralAI } from "@langchain/mistralai"
 
-export async function testAi() {
-    const model = new ChatGoogleGenerativeAI({
-        model: "gemini-2.5-flash-lite",
-        apiKey: process.env.GOOGLE_API_KEY
-    });
 
-    const response = await model.invoke("Hii my name is Pocha ");
-    console.log(response.text);
+const Geminimodel = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash-lite",
+    apiKey: process.env.GOOGLE_API_KEY
+});
+
+const Mistralmodel = new ChatMistralAI({
+    model: "mistral-small-latest",
+    apiKey: process.env.MISTRAL_API_KEY
+});
+
+export async function generateResponse(messages) {
+    const response = await Geminimodel.invoke(messages.map(msg => {
+        if(msg.role === 'user') {
+            return new HumanMessage(msg.content);
+        } else {
+            return new AIMessage(msg.content);
+        }
+    }));
+    return response.text;
 }
+
+export async function generateChatTitle(message) {
+    const response = await Mistralmodel.invoke([
+        new SystemMessage(`Generate a concise title for the following conversation.`),
+        new HumanMessage(`Generate a concise title for the following conversation: ${message}`)
+    ]);
+    return response.text;
+}   
---
+++ DAY_18_PERPLEXITY_AI/frontend/src/features/auth/hook/useChat.js
@@ -0,0 +1,6 @@
+import { initSocketConnection } from "../../chat/service/chat.socket";
+
+export const useChat = () => {
+
+    return { initSocketConnection };
+}
